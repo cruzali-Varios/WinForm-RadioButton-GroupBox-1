d45b652b909f037b454ad884024dc0d2f5cd4151
Ali <ali.cruz@me.com>
2023-05-05 11:11:05 -0600

    Carga inicial, proyecto inconcluso

---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelsRaiosGroupBoxDinamicos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            VScrollBar vScrollBar = new VScrollBar();
+            vScrollBar.Dock = DockStyle.Right;
+            Controls.Add(vScrollBar);
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            int xGB = 50;
+            int yGB = 80;
+
+            int xRB = 50;
+            int yRB = 80;
+
+            Point positionGB = new Point(xGB, yGB);
+
+            List<GroupBox> listGB = new List<GroupBox>();
+            List<RadioButton> listRB = new List<RadioButton>();
+
+
+            for (int i = 1; i <= 2; i++)
+            {
+                GroupBox newGB = new GroupBox();
+
+                newGB.Name = "GroupBox-" + i;
+                newGB.BackColor = Color.FromArgb(79, 239, 255);
+                newGB.ForeColor = Color.Maroon;
+                newGB.Text = "GroupBox-" + i;
+                newGB.Font = new Font("Georgia", 12);
+                newGB.FlatStyle = FlatStyle.Flat;
+                newGB.Width = 650;
+                newGB.Height = 150;
+                this.Controls.Add(newGB);
+                newGB.Location = new Point(xGB, yGB += 170);
+
+                listGB.Add(newGB);
+
+                Console.WriteLine("*GB\n");
+
+                for (int j = 1; j <= 4; j++)
+                {
+                    RadioButton rdbtn = new RadioButton();
+
+                    rdbtn.Name = "Radio-" + j;
+                    rdbtn.Text = "Radio-" + j;
+                    newGB.Controls.Add(rdbtn);
+                    rdbtn.Location = new Point(xRB += 105, yRB);
+
+                    listRB.Add(rdbtn);
+
+                    Console.WriteLine("RB");
+
+                }
+
+                xRB = 50;
+                Console.WriteLine("\n========\n");
+
+
+            }
+
+            Console.WriteLine("FIN de la creacion de controles\n");
+
+            Console.WriteLine("Lectura de controles dinamicos:");
+
+            foreach (Control gb in listGB)
+            {
+                foreach (Control rb in listRB)
+                {
+                    Console.WriteLine("GB.Name = " + gb.Name + ". RB.Name = " + rb.Name);
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("groupBox1.Name = " + groupBox1.Name.ToString());
+
+            Console.WriteLine("\nradioButton3.Name = " + rbt88.Name.ToString());
+
+            Console.WriteLine("\ngroupBox1.Controls.Count = " + groupBox1.Controls.Count);
+
+            Console.WriteLine("=================");
+            foreach (Control control in groupBox1.Controls)
+            {
+                Console.WriteLine("control.Name = " + control.Name.ToString());
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace LabelsRaiosGroupBoxDinamicos
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbt1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(146, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // rbt1
+            // 
+            this.rbt1.AutoSize = true;
+            this.rbt1.ForeColor = System.Drawing.Color.Maroon;
+            this.rbt1.Location = new System.Drawing.Point(380, 106);
+            this.rbt1.Name = "rbt1";
+            this.rbt1.Size = new System.Drawing.Size(85, 17);
+            this.rbt1.TabIndex = 1;
+            this.rbt1.TabStop = true;
+            this.rbt1.Text = "radioButton1";
+            this.rbt1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbt1;
+    }
+}
